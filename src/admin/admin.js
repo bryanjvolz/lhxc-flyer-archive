f058cf37cff30784573569fd348fc990f5d64975
Bryan Volz <bryan@typocracy.net>
2025-03-12 21:41:19 -0400

    Fixes for new venue switcher

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -5,23 +5,39 @@ jQuery(document).ready(function($) {
 
         if ($input.is(':hidden')) {
             $select.hide();
-            $input.show();
+            $input.show().attr('name', $select.attr('name'));
+            $select.removeAttr('name');
             $(this).text('Use Existing Venue');
         } else {
-            $select.show();
-            $input.hide();
+            $select.show().attr('name', $input.attr('name'));
+            $input.hide().removeAttr('name');
             $(this).text('Add New Venue');
         }
     });
 
-    // Handle form submission
-    $('body').on('submit', 'form.compat-item', function() {
-        $('.new-venue-input:visible').each(function() {
-            var $input = $(this);
-            var $select = $input.siblings('.venue-select');
-            if ($input.val()) {
-                $select.val($input.val());
+    // Update form submission handling
+    $('body').on('submit', 'form.compat-item', function(e) {
+        e.preventDefault();
+
+        var $form = $(this);
+        var $input = $form.find('.new-venue-input:visible');
+        var $select = $form.find('.venue-select');
+
+        if ($input.length && $input.val()) {
+            var newVenue = $input.val();
+
+            // Update hidden input with new venue value
+            if (!$form.find('input[name="new_venue"]').length) {
+                $form.append('<input type="hidden" name="new_venue" value="' + newVenue + '">');
+            } else {
+                $form.find('input[name="new_venue"]').val(newVenue);
             }
-        });
+
+            // Update select value
+            $select.val(newVenue);
+        }
+
+        // Submit the form
+        $form[0].submit();
     });
 });
